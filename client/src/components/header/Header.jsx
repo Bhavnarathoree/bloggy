b5f8925e8b014cac0c30a79d8225ddfb1241909d
BhavnaRathore <121177098+BhavnaRathore@users.noreply.github.com>
2023-03-23 21:27:42 +0530

    second commit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,10 +4,11 @@ export default function Header() {
   return (
     <div className='header'>
       <div className="headerTitles">
-        <span className="headerTitlesm"> React & Node </span>
+         
         <span className="headerTitlelg">Bloggy</span>
+        <span className="headerTitlesm"> A New World of Blogs</span>
       </div>
-      <img className="headerImg" src="https://images.unsplash.com/photo-1552083375-1447ce886485?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwd2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt=""/>
+      <img className="headerImg" src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
     </div>
   )
 }
